9025e18a09938ac378f732b9a2e3fb18f5e262d7
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2018-05-07 12:30:18 -0400

    Subclass Swift from Obj-C in main target
    Not compiling because SWIFT_CLASS decl acts as public rather than open
    by default:
    
        #define SWIFT_CLASS(SWIFT_NAME) SWIFT_RUNTIME_NAME(SWIFT_NAME) \
            __attribute__((objc_subclassing_restricted)) \
            SWIFT_CLASS_EXTRA
    
    SWIFT_CLASS_EXTRA is empty by default, but I bet you could define
    before the import to do Evil Things.
    
    SWIFT_RUNTIME_NAME expands out to
    
        __attribute__((objc_runtime_name(X)))
    
    Which is how the name-mangling is managed at runtime without cluttering
    up source code.

---
+++ MixedLanguageTesting/ObjCSubclassOfSwiftClass.m
@@ -0,0 +1,13 @@
+//
+//  ObjCSubclassOfSwiftClass.m
+//  MixedLanguageTesting
+//
+//  Created by Jeremy W. Sherman on 2018-05-07.
+//  Copyright © 2018 Jeremy W. Sherman. All rights reserved.
+//
+
+#import "ObjCSubclassOfSwiftClass.h"
+
+@implementation ObjCSubclassOfSwiftClass
+
+@end
--- MixedLanguageTestingTests/ObjCTests.m
+++ MixedLanguageTestingTests/ObjCTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "MixedLanguageTesting-Swift.h"
 
 @interface ObjCTests : XCTestCase
 
